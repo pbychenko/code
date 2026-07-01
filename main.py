c6fdb23af44a9685c6664b0b9f4e4c8e07bf8421
Pavel Bychenko <pavelv87@gmail.com>
2026-07-01 07:15:29 +0500

    fix changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Cookie
 from environs import Env
-from models import User, Feedback
+from models import User, Feedback, UserCreate
 
 
 
@@ -37,3 +37,85 @@ def read_root(feedback: Feedback, is_premium: bool = False):
     if is_premium:
         responce_message += " Ваш отзыв будет рассмотрен в приоритетном порядке.."
     return { "message": responce_message }
+
+@app.post("/create_user")
+def read_root(user_create: UserCreate):
+    return user_create
+
+sample_product_1 = {
+    "product_id": 123,
+    "name": "Smartphone",
+    "category": "Electronics",
+    "price": 599.99
+}
+
+sample_product_2 = {
+        "product_id": 456,
+        "name": "Phone Case",
+        "category": "Accessories",
+        "price": 19.99
+    }
+
+sample_product_3 = {
+    "product_id": 789,
+    "name": "Iphone",
+    "category": "Electronics",
+    "price": 1299.99
+}
+
+sample_product_4 = {
+    "product_id": 101,
+    "name": "Headphones",
+    "category": "Accessories",
+    "price": 99.99
+}
+
+sample_product_5 = {
+    "product_id": 202,
+    "name": "Smartwatch",
+    "category": "Electronics",
+    "price": 299.99
+}
+
+sample_products = [sample_product_1, sample_product_2, sample_product_3, sample_product_4, sample_product_5]
+
+@app.get("/product/{product_id}")
+def read_root(product_id: int):
+    for product in sample_products:
+        if product["product_id"] == product_id:
+            return product
+
+    return {"error": "Product not found"} 
+
+@app.get("/products/search")
+def read_root(keyword: str, category: str | None = None, limit: int | None = 10):
+    result = []
+    for product in sample_products:
+        if product["category"] == category and keyword.lower() in product["name"].lower():
+            result.append(product)
+
+    return result[:limit]
+
+import uuid
+id = ''
+user_data = {
+    "username": "user123",
+    "password": "password123"
+}
+
+@app.post("/login")
+def read_root(user: User, response: Response):
+    print(user)
+    if user.username == "user123" and user.password == "password123":
+        global id
+        id= uuid.uuid4()
+        response.set_cookie(key="session_token", value=id, max_age=3600, httponly=True)
+
+        return {"message": "Login successful"}
+
+@app.get("/user")
+def read_root(session_token = Cookie()):
+    if session_token and session_token == str(id):
+        return user_data
+    
+    return {"error": "invalid_token_value"}
--- models.py
+++ models.py
@@ -1,15 +1,27 @@
+from typing import Optional
 from pydantic import BaseModel,Field, EmailStr
 
 
+# class User(BaseModel):
+#     age: int
+#     name: str
 class User(BaseModel):
-    age: int
+    username: str
+    password: str
+
+class UserCreate(BaseModel):
+    email: EmailStr
     name: str
+    age: int | None
+    is_subscribed: bool  | None
 
 class Contact(BaseModel):
     email: EmailStr
-    phone: int = Field(..., gt=0, lt=10000000000)  # Example: phone number must be a positive integer less than 10 billion
+    phone: str | None = Field(
+        default=None,
+        pattern=r"^\d+$", min_length=7, max_length=15) 
     
 class Feedback(BaseModel):
     message: str = Field(min_length=10, max_length=500)
     name: str = Field(min_length=2, max_length=50)
-    # contact: Contact
+    contact: Contact
